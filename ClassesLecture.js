4f30c23c01de00bce49ec23652d5fecb6739da46
femioyedepo <femoye@gmail.com>
2019-10-19 14:03:41 -0500

    Created an Employee Class

--- ClassesLecture.js
+++ ClassesLecture.js
@@ -1,2 +1,12 @@
-// create an array of employees
+// create an empty array of employees
 const employeeDB = [];
+
+// create a class for Employee
+class Employee {
+    constructor (fname, lname, jobTitle, department) {
+        this.fname = fname;
+        this.lname = lname;
+        this.jobTitle = jobTitle;
+        this.department = department;
+    }
+}

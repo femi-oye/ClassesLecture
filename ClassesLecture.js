d1dcbb05cd95a8d258f881af7eec2c5766b2111d
femioyedepo <femoye@gmail.com>
2019-10-19 14:08:08 -0500

    Created a function that creates multiple instances of an Employee

--- ClassesLecture.js
+++ ClassesLecture.js
@@ -13,3 +13,23 @@ class Employee {
 
 // create an instance of the Employee Class
 femi = new Employee("Femi", "Oye", "salesforce", "1234")
+
+// create a function that creates multiple instances of an Employee
+function addEmployee (employeeFirstName, employeeLastName, employeeJobTitle, employeeDept) {
+    let newEmployee = new Employee (employeeFirstName, employeeLastName, employeeJobTitle, employeeDept);
+    employeeDB.push(newEmployee);
+
+    return newEmployee;
+
+}
+
+addEmployee("Femi", "Oye", "Salesforce", "1234");
+addEmployee("Bode", "Falade", "Developer", "2345");
+addEmployee("Jim", "Jones", "HR Manager", "9805");
+addEmployee("John", "Brown", "Sales Manager", "0001");
+addEmployee("Kim", "Frye", "Developer", "0453");
+addEmployee("Brad", "Smith", "IT Manager", "3960");
+addEmployee("Joko", "Sule", "Bus Conductor", "0032");
+addEmployee("Gideon", "Joker", "Baby", "0518");
+addEmployee("Ade", "Owolabi", "Health", "9922");
+addEmployee("Emmanuel", "Anthony", "Cook", "4501");
